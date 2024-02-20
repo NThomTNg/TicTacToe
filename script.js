fe6485fd2d554864695fa922fbfe1cb87ca32944
Nhan Thomas Trong Nguyen <112351775+NThomTNg@users.noreply.github.com>
2024-02-20 21:53:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#textStatus");
 const restartBtn = document.querySelector("#restart");
-const winConditions = [
+const winConditions = [ // Defining the win condition of the game
 
     [0, 1, 2],
     [3, 4, 5],
@@ -14,78 +14,79 @@ const winConditions = [
 
 ];
 
-let options = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
+let options = ["", "", "", "", "", "", "", "", ""]; // Options for clicking on the board
+let currentPlayer = "X"; 
 let running = false;
 
-startGame();
+startGame(); 
 
-function startGame(){
+function startGame(){ // Function to start the game
     console.log("Game started");  
-    cells.forEach(cell => cell.addEventListener("click", cellClicked))
-    restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.addEventListener("click", cellClicked)) // Register that a cell is clicke
+    restartBtn.addEventListener("click", restartGame); // Restart button
+    statusText.textContent = `${currentPlayer}'s turn`; // Displays whos turn
     running = true;
 }
 
-function cellClicked() {
+function cellClicked() { // Function for when a cell is clicked
     console.log("Cell clicked");  
-    const cellIndex = parseInt(this.getAttribute('cellIndex'));
+    const cellIndex = parseInt(this.getAttribute('cellIndex')); // Gets the attribute from the cell that is clicked and converts to int
 
-    if (options[cellIndex] !== "" || !running) {
+    if (options[cellIndex] !== "" || !running) { // Stops the function if the cell array is not empty or if the game is not running
         return;
     }
     
-    updateCell(this, cellIndex);
-    checkWinner();
+    updateCell(this, cellIndex); // Calls the updateCell function to update the cell
+    checkWinner(); // Calls the checkWinner function
 }
 
 
 
-function updateCell(cell, index){
+function updateCell(cell, index){ // Function to update the current player cell
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
 }
 
-function changePlayer(){
+function changePlayer(){ // Function to swap player after each turn
     currentPlayer = (currentPlayer == "X") ? "O" : "X";
     textStatus.textContent = `${currentPlayer}'s turn`;
 }
 
-function checkWinner(){
-    let roundWon = false;
+function checkWinner(){ // Function to check the winner of the game
+    let roundWon = false; // Starts as false
 
-    for(let i = 0; i < winConditions.length; i++){
+    for(let i = 0; i < winConditions.length; i++){ // For loop for winning condition
         const condition = winConditions[i];
+        // Values from option array
         const A = options[condition[0]];
         const B = options[condition[1]];
-        const C = options[condition[2]];
+        const C = options[condition[2]]; 
 
-        if(A == "" || B == "" || C == ""){
+        if(A == "" || B == "" || C == ""){ // Checks if the values are empty string
             continue;
         }
-        if(A == B && B == C){
+        if(A == B && B == C){ // Checks if the values are the same and if they are, it sets to roundWon
             roundWon = true;
             break;
         }
     }
 
-    if(roundWon){
+    if(roundWon){ // If roundWon is true, displays the player who won
         textStatus.textContent = `${currentPlayer} is the big winner!`;
         running = false;
     }
-    else if (!options.includes("")){
+    else if (!options.includes("")){ // If all cells are clicked and there are no empty strings in the option, it displays draw
         textStatus.textContent = `Thats a draw!`;
         running = false;
     }
-    else {
+    else { // If both conditions above are met, changePlayer
         changePlayer();
     }
 }
 
-function restartGame(){
-    currentPlayer = "X";
-    options = ["", "", "", "", "", "", "", "", ""];
+function restartGame(){ // Function for restarting the game
+    currentPlayer = "X"; // Player X starts
+    options = ["", "", "", "", "", "", "", "", ""]; // Empty the board
     textStatus.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
     running = true;
